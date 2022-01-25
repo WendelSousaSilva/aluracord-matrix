68bc3e44d767747401fd52974968782eebf6c577
WendelSousaSilva <wendelssousa@gmail.com>
2022-01-25 11:46:10 -0300

    segundo commit

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from '../config.json';
+import {useRouter} from 'next/router';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-  }
+
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -60,11 +35,14 @@ function Title(props) {
  // export default HomePage
 
  export default function PaginaInicial() {
-    const username = 'WendelSousaSilva';
+    //const username = 'WendelSousaSilva';
+    const[ username, setUsername] = React.useState('WendelSousaSilva'); 
+    const roteamento = useRouter();
+    
   
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +69,12 @@ function Title(props) {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit ={function (infoDoEvento) {
+                infoDoEvento.preventDefault();
+                console.log('Alguem submeteu o form!');
+                roteamento.push('/chat');
+                //window.location.href = '/chat'; 
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,8 +84,18 @@ function Title(props) {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
-              <TextField
+              <input 
+              type="text"
+              value ={username}
+              onChange={function (event){
+                console.log('Usuario digitou!', event.target.value);
+                //onde esta o valor?
+                const valor = event.target.value;
+                //troca o valor da variavel
+                setUsername(valor);
+
+              }} />
+              {/* <TextField
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -111,7 +105,7 @@ function Title(props) {
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
-              />
+              /> */}
               <Button
                 type='submit'
                 label='Entrar'
